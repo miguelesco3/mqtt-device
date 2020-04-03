f83a560deacb350077451f0136a4c06d2618bd12
Miguel <miguelesco3@gmail.com>
2020-04-03 10:37:55 -0500

    Create Main and update gitignore

---
+++ include/constants.h
@@ -0,0 +1,17 @@
+#define SSID "Miguel"
+#define SSID_PASSWORD "0987654321"
+
+#define MQTT_ID "esp32"
+#define MQTT_USER "Arduino"
+#define MQTT_PASSWORD "PASSWORD"
+
+// #define BROKER "192.168.43.1"
+#define BROKER "192.168.43.155"
+
+#define REPORT_FREQ 20000
+#define MAX_ALARM_FREQ 5000
+
+#define INPUT_BTN 23
+
+#define PIXEL_COUNT 8
+#define PIXEL_PIN 9
