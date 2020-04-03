c91f396f7c5f86d3a082affc80f5b963abc1e29d
Miguel <miguelesco3@gmail.com>
2020-04-03 10:41:43 -0500

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -8,10 +8,5 @@
 // #define BROKER "192.168.43.1"
 #define BROKER "192.168.43.155"
 
-#define REPORT_FREQ 20000
-#define MAX_ALARM_FREQ 5000
 
-#define INPUT_BTN 23
-
-#define PIXEL_COUNT 8
-#define PIXEL_PIN 9
+#define INPUT_SENSOR 23
