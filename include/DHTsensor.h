57e28ff9b090e8e25edc6db9e7372c462448c391
Miguel <miguelesco3@gmail.com>
2020-04-15 11:17:25 -0500

    Cambios para vincular código con receptor en python

--- include/DHTsensor.h
+++ include/DHTsensor.h
@@ -11,6 +11,7 @@ public:
     DHTsensor(uint16_t device_id, uint16_t threshold, long sample_time): InputDevice(device_id, threshold, sample_rate) {};
     bool init(uint8_t pin);
     uint16_t read();
+    uint16_t readhum();
 };
 
 bool DHTsensor::init(uint8_t pin){
@@ -21,4 +22,8 @@ bool DHTsensor::init(uint8_t pin){
 uint16_t DHTsensor::read(){
     float t = dht.readTemperature();
         return (t);
+}
+uint16_t DHTsensor::readhum(){
+    float h = dht.readHumidity();
+        return (h);
 }
